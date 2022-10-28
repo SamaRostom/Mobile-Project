627422659cc9a3e2c171ccc3e865187059e3efd6
SamaRostom <sama1904157@miuegypt.edu.eg>
2022-10-28 15:12:40 +0200

    Update main.dart

--- Hangtoe/lib/main.dart
+++ Hangtoe/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+// import 'package:flutter/Home.dart';
 // import 'dart:ui';
-
+import 'package:flutter_application_1/Home.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -16,79 +17,14 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomeScreen(),
+      home: const HomeScreen(),
+      // initialRoute: '/',
+      // routes: {
+      //   // '/': (context) => const HomeScreen(),
+      //   // '/details': (context) => const DetailScreen(),
+      //   // '/third': (context) => const ThirdScreen(),
+      // },
     );
   }
 }
 
-class MyHomeScreen extends StatelessWidget {
-  const MyHomeScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Column(
-          // mainAxisAlignment: MainAxisAlignment.spaceAround,
-          // mainAxisSize: MainAxisSize.max,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Icon(Icons.account_circle, size: 50),
-                Column(children: const [
-                  Text(
-                    'Flutter McFlutter',
-                    style: TextStyle(
-                      fontSize: 30,
-                    ),
-                  ),
-                  Text(
-                    'Experienced App Developer',
-                    style: TextStyle(
-                      fontSize: 20,
-                    ),
-                  ),
-                ])
-              ],
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              mainAxisSize: MainAxisSize.max,
-              children: const [
-                Text(
-                  '123 Main Street',
-                  style: TextStyle(
-                    fontSize: 20,
-                  ),
-                ),
-                Text(
-                  '(415) 555-0198',
-                  style: TextStyle(
-                    fontSize: 20,
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              mainAxisSize: MainAxisSize.max,
-              children: const [
-                Icon(Icons.accessibility, size: 50),
-                Icon(Icons.timer, size: 50),
-                Icon(Icons.phone_android, size: 50),
-                Icon(Icons.phone_iphone, size: 50)
-              ],
-            )
-          ],
-        ),
-      ),
-    );
-  }
-}
