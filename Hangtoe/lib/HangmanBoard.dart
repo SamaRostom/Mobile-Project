6e27f47eb7feb1746c84c75de612b3d380745753
SalmaSalahh <salma1905780@miuegypt.edu.eg>
2022-10-29 12:39:04 +0200

    Update HangmanBoard.dart

--- Hangtoe/lib/HangmanBoard.dart
+++ Hangtoe/lib/HangmanBoard.dart
@@ -1,6 +1,5 @@
-
 import 'package:flutter/material.dart';
-
+import 'package:flutter_application_1/data.dart';
 
 class HangmanBoard extends StatefulWidget {
   const HangmanBoard({super.key});
@@ -10,38 +9,37 @@ class HangmanBoard extends StatefulWidget {
   _HangmanBoardState createState() => _HangmanBoardState();
 }
 
-
 class _HangmanBoardState extends State<HangmanBoard>{
   @override
-  //choosing the game word
-  String word = "Flutter".toUpperCase();
-  //Create a list that contains the Alphabet, or you can just copy and paste it
-  List<String> alphabets = [
-    "A",
-    "B",
-    "C",
-    "D",
-    "E",
-    "F",
-    "G",
-    "H",
-    "I",
-    "J",
-    "K",
-    "L",
-    "M",
-    "N",
-    "O",
-    "P",
-    "Q",
-    "R",
-    "S",
-    "T",
-    "U",
-    "V",
-    "W",
-    "X",
-    "Y",
-    "Z"
-  ];
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          //choosing the game word
+          //   List<String> getAlphebat() {
+          //   List<String> alphabets = [];
+          //   for (int i = 65; i <= 90; i++) {
+          //     alphabets.add(String.fromCharCode(i));
+          //   }
+          //   return alphabets;
+          // }
+          children: [
+            //for (int i = 65; i <= 90; i++){
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder( //to set border radius to button
+                  borderRadius: BorderRadius.circular(20)
+                ),
+              ),
+              onPressed: () {}, 
+              child: const Text('Hangman',style: TextStyle(fontSize: 20,),),
+            ),
+          //}  
+          ],
+        ),
+      ),
+    );
+  }
 }
+
+
