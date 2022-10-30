a0bf2c56daeed33a5288cd29d4887330db589197
SalmaSalahh <salma1905780@miuegypt.edu.eg>
2022-10-30 20:01:20 +0200

    Update HangmanBoard.dart

--- Hangtoe/lib/HangmanBoard.dart
+++ Hangtoe/lib/HangmanBoard.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+// import 'dart:math';
 import 'package:flutter_application_1/data.dart';
 import 'package:flutter_application_1/main.dart';
 import 'package:flutter_application_1/letter.dart';
@@ -7,48 +8,27 @@ import 'package:flutter_application_1/figure_image.dart';
 
 class HangmanBoard extends StatefulWidget {
   const HangmanBoard({super.key});
-
   @override
   // ignore: library_private_types_in_public_api
   _HangmanBoardState createState() => _HangmanBoardState();
 }
 
-// class _HangmanBoardState extends State<HangmanBoard>{
-//   @override
-//   Widget build(BuildContext context) {
-//     return SafeArea(
-//       child: Scaffold(
-//         body: Column(
-//           //choosing the game word
-//           //   List<String> getAlphebat() {
-//           //   List<String> alphabets = [];
-//           //   for (int i = 65; i <= 90; i++) {
-//           //     alphabets.add(String.fromCharCode(i));
-//           //   }
-//           //   return alphabets;
-//           // }
-//           children: [
-//             //for (int i = 65; i <= 90; i++){
-//             ElevatedButton(
-//               style: ElevatedButton.styleFrom(
-//                 shape: RoundedRectangleBorder( //to set border radius to button
-//                   borderRadius: BorderRadius.circular(20)
-//                 ),
-//               ),
-//               onPressed: () {},
-//               child: const Text('Hangman',style: TextStyle(fontSize: 20,),),
-//             ),
-//           //}
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
-
 class _HangmanBoardState extends State<HangmanBoard> {
+  
+  // String word = Data.wordList[rnd.nextInt(Data.wordList.length)];
   //choosing the game word
-  String word = "Flutter".toUpperCase();
+  // String word = "Flutter".toUpperCase();
+  // String word = Data.wordList[0].toUpperCase();
+  if(Data.cc == "Animals"){
+
+  }
+  else if(Data.cc == "Sports"){
+
+  }
+  else{
+    
+  }
+  // String word = randomListItem(Data.wordList);
   //Create a list that contains the Alphabet, or you can just copy and paste it
   List<String> alphabets = [
     "A",
