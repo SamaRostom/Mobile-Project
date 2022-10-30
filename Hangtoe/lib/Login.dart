04efb26f6998b34fdc663d6ec7af59af4bee9320
ReemAbdulraouf <76842378+ReemAbdulraouf@users.noreply.github.com>
2022-10-30 19:36:22 +0200

    Create Login.dart

---
+++ Hangtoe/lib/Login.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/main.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_application_1/data.dart';
+
+class Login extends StatefulWidget {
+  const Login({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        padding: const EdgeInsets.all(8),
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            // ignore: prefer_const_literals_to_create_immutables
+            children: [
+              const SizedBox(
+                height: 100,
+              ),
+              Text(
+                'Login',
+                style:
+                    GoogleFonts.patrickHand(fontSize: 75, color: Colors.white),
+              ),
+            ],
+          ),
+          Column(
+            children: [
+              Image.asset('assets/Login.png', width: 250),
+              const TextField(
+                style: TextStyle(color: Colors.white),
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Enter the username',
+                ),
+              ),
+              const SizedBox(height: 50),
+              const TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Enter the Password',
+                ),
+              ),
+              const SizedBox(height: 50),
+              ButtonBar(
+                children: <Widget>[
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/');
+                    },
+                    child: const Text('Login'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/Login');
+                    },
+                    child: const Text('Cancel'),
+                  )
+                ],
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
