9f655327ad7da386ee9a9f9d52ad8dc7f78f13c9
SalmaSalahh <salma1905780@miuegypt.edu.eg>
2022-10-30 21:31:32 +0200

    Update data.dart

--- Hangtoe/lib/data.dart
+++ Hangtoe/lib/data.dart
@@ -140,4 +140,32 @@ class Data {
       'Countries': 'China',
     },
   ];
+  static List<String> alphabets = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z"
+  ];
 }
