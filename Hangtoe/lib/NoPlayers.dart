eb70e8ec039f990b8dc242874a9f43c8997b85b0
YaraLotfy <96872220+YaraLotfy@users.noreply.github.com>
2022-10-31 00:31:00 +0200

    Update NoPlayers.dart

--- Hangtoe/lib/NoPlayers.dart
+++ Hangtoe/lib/NoPlayers.dart
@@ -1,25 +1,30 @@
 import 'package:flutter/material.dart';
+
 import 'package:flutter_application_1/data.dart';
+
 import 'package:google_fonts/google_fonts.dart';
 
-class PName extends StatefulWidget {
-  const PName({super.key});
+class Players extends StatefulWidget {
+  const Players({super.key});
 
   @override
 
   // ignore: library_private_types_in_public_api
 
-  _PNameState createState() => _PNameState();
+  _PlayersState createState() => _PlayersState();
 }
 
-class _PNameState extends State<PName> {
+class _PlayersState extends State<Players> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: ListView(
-          padding: const EdgeInsets.all(8),
-          children: [
+        body: Column(
+          // crossAxisAlignment: CrossAxisAlignment.center,
+
+          // mainAxisSize: MainAxisSize.max,
+
+          children: <Widget>[
             IconButton(
               icon: const Icon(Icons.arrow_back),
               color: Colors.white,
@@ -27,117 +32,87 @@ class _PNameState extends State<PName> {
                 Navigator.pop(context);
               },
             ),
+            Image.asset(
+              'assets/Friends.png',
+              width: 200,
+              height: 200,
+            ),
             const SizedBox(
               height: 50,
             ),
             Text(
-              'TO Start The Game please Enter The Players Name',
+              "Choose The Number Of Players To Start The Game",
               textAlign: TextAlign.center,
-              style: GoogleFonts.patrickHand(fontSize: 25, color: Colors.white),
+              style: GoogleFonts.patrickHand(fontSize: 35, color: Colors.white),
             ),
             const SizedBox(
-              height: 100,
+              height: 50,
             ),
-            Data.player == 2
-                ? Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      const TextField(
-                        style: TextStyle(color: Colors.white),
-                        decoration: InputDecoration(
-                          fillColor: Colors.white,
-                          focusColor: Colors.white,
-                          hoverColor: Colors.white,
-                          enabledBorder: OutlineInputBorder(
-                            borderSide:
-                                BorderSide(width: 3, color: Colors.white),
-                          ),
-                          hintText: 'Enter 1 Player Name',
-                          hintStyle: TextStyle(color: Colors.white),
-                        ),
-                      ),
-                      const SizedBox(height: 20),
-                      const TextField(
-                        style: TextStyle(color: Colors.white),
-                        decoration: InputDecoration(
-                          enabledBorder: OutlineInputBorder(
-                            borderSide:
-                                BorderSide(width: 3, color: Colors.white),
-                          ),
-                          hintText: 'Enter 2 Player Name',
-                          hintStyle: TextStyle(color: Colors.white),
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 70,
-                      ),
-                      SizedBox(
-                        height: 50, //height of button
-                        width: 120,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 80, //height of button
 
-                        child: ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            shape: RoundedRectangleBorder(
-                                //to set border radius to button
-                                borderRadius: BorderRadius.circular(20)),
-                          ),
-                          onPressed: () {
-                            Navigator.pushNamed(context, '/gamehome');
-                          },
-                          child: Text('Play',
-                              style: GoogleFonts.patrickHand(
-                                fontSize: 25,
-                              )),
-                        ),
+                  width: 200, //width of button
+
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+
+                          //to set border radius to button
+
+                          borderRadius: BorderRadius.circular(20)),
+                    ),
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/PNames');
+
+                      Data.player = 1;
+                    },
+                    child: const Text(
+                      '1 Player',
+                      style: TextStyle(
+                        fontSize: 20,
                       ),
-                    ],
-                  )
-                : Column(
-                    children: [
-                      const SizedBox(
-                        height: 40,
-                      ),
-                      const TextField(
-                        style: TextStyle(color: Colors.white),
-                        decoration: InputDecoration(
-                          // border: OutlineInputBorder(),
-
-                          enabledBorder: OutlineInputBorder(
-                            borderSide:
-                                BorderSide(width: 3, color: Colors.white),
-                          ),
-
-                          hintText: 'Enter Player Name',
-                          hintStyle: TextStyle(color: Colors.white),
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 70,
-                      ),
-                      SizedBox(
-                        height: 50, //height of button
-
-                        width: 120,
-
-                        child: ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            shape: RoundedRectangleBorder(
-
-                                //to set border radius to button
-
-                                borderRadius: BorderRadius.circular(20)),
-                          ),
-                          onPressed: () {
-                            Navigator.pushNamed(context, '/gamehome');
-                          },
-                          child: Text('Play',
-                              style: GoogleFonts.patrickHand(
-                                fontSize: 25,
-                              )),
-                        ),
-                      ),
-                    ],
+                    ),
                   ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 35,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 80, //height of button
+
+                  width: 200, //width of button
+
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+
+                          //to set border radius to button
+
+                          borderRadius: BorderRadius.circular(20)),
+                    ),
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/PNames');
+
+                      Data.player = 2;
+                    },
+                    child: const Text(
+                      '2 Players',
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ],
         ),
       ),
