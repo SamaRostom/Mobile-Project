730ddc2f27b254692ae1354e64192935ed370760
SamaRostom <sama1904157@miuegypt.edu.eg>
2022-10-30 02:12:40 +0200

    Update howtoplay.dart

--- Hangtoe/lib/howtoplay.dart
+++ Hangtoe/lib/howtoplay.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/data.dart';
 
@@ -30,22 +32,41 @@ class Howtoplay extends StatelessWidget {
                 ],
               ),
             const SizedBox(
-              height: 90,
+              height: 100,
             ),
             Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+              // mainAxisAlignment: MainAxisAlignment.start,
               children: 
                [
                 Data.chosed == 'hangman'?
-                const Text(
-                  'In the game of Hangman, a clue word is given by the program that the player has to guess , letter by letter and if the player selects an incorrect letter, one part of the hangman body is drawn. If the full body is drawn the player loses, otherwise the correct letter is found and the player wins.',
+                const Text('''
+          In the game of Hangman, 
+          a clue word is given by 
+          the program that the player
+          has to guess , letter by 
+          letter and if the player 
+          selects an incorrect letter, 
+          one part of the hangman body 
+          is drawn. If the full body 
+          is drawn the player loses, 
+          otherwise the correct letter 
+          is found and the player wins.''',
                   style: TextStyle(
                         fontSize: 25,
                         color: Colors.white,
                       ),
                 ):
                 const Text(
-                  ' Tic-tac-toe is a puzzle game for two players, X and O, who take turns marking the spaces in a 3×3 grid. The player who succeeds in placing three of their marks in a horizontal, vertical, or diagonal row wins the game.',
+                  '''
+      Tic-tac-toe is a puzzle game 
+      for two players, X and O, 
+      who take turns marking the 
+      spaces in a 3×3 grid. The 
+      player who succeeds in placing 
+      three of their marks in a 
+      horizontal, vertical, or diagonal 
+      row wins the game.
+          ''',
                   style: TextStyle(
                         fontSize: 25,
                         color: Colors.white,
