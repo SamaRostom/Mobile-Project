18042e7f7b320a16b9a5853096b887b3c94adc9d
ReemAbdulraouf <76842378+ReemAbdulraouf@users.noreply.github.com>
2022-10-31 00:04:47 +0200

    Update Login.dart

--- Hangtoe/lib/Login.dart
+++ Hangtoe/lib/Login.dart
@@ -5,13 +5,13 @@ import 'package:flutter_application_1/data.dart';
 
 class Login extends StatefulWidget {
   const Login({super.key});
-
   @override
   // ignore: library_private_types_in_public_api
   _LoginState createState() => _LoginState();
 }
 
 class _LoginState extends State<Login> {
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,41 +32,61 @@ class _LoginState extends State<Login> {
               ),
             ],
           ),
-          Column(
-            children: [
-              Image.asset('assets/Login.png', width: 250),
-              const TextField(
-                style: TextStyle(color: Colors.white),
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Enter the username',
-                ),
-              ),
-              const SizedBox(height: 50),
-              const TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Enter the Password',
-                ),
-              ),
-              const SizedBox(height: 20),
-              ButtonBar(
-                children: <Widget>[
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/');
-                    },
-                    child: const Text('Login'),
+          Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                Image.asset('assets/Login.png', width: 250),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Enter the Username',
+                    hintStyle: TextStyle(color: Colors.white),
                   ),
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/');
-                    },
-                    child: const Text('Cancel'),
-                  )
-                ],
-              )
-            ],
+                  autofocus: true,
+                  validator: (val) {
+                    if (val != null && val.isNotEmpty) {
+                      return null;
+                    } else {
+                      return 'Please enter a username';
+                    }
+                  },
+                ),
+                const SizedBox(height: 50),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Enter the Password',
+                    hintStyle: TextStyle(color: Colors.white),
+                  ),
+                  autofocus: true,
+                  validator: (val) {
+                    if (val != null && val.isNotEmpty) {
+                      return null;
+                    } else {
+                      return 'Please enter a password';
+                    }
+                  },
+                ),
+                const SizedBox(height: 20),
+                ButtonBar(
+                  children: <Widget>[
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/');
+                      },
+                      child: const Text('Login'),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/Login');
+                      },
+                      child: const Text('Cancel'),
+                    )
+                  ],
+                )
+              ],
+            ),
           ),
         ],
       ),
