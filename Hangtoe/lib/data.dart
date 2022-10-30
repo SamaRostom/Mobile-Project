4d3ba6e2cc27f65183d6458fd5aaf9b06fca5df4
SalmaSalahh <salma1905780@miuegypt.edu.eg>
2022-10-30 12:00:42 +0200

    Update data.dart

--- Hangtoe/lib/data.dart
+++ Hangtoe/lib/data.dart
@@ -2,7 +2,13 @@ import 'package:flutter/material.dart';
 
 class Data {
   //adding the number of tries
-  static int tries = 5;
+  static int tries = 0;
   static List<String> selectedChar = [];
   static var chosed; 
+  static Color primaryColorDark = Color(0xFF231954);
+  // static var draw;
+  // static List<String> Categories = [
+  //   Animals,
+
+  // ];
 }
