1ca8f451c94897880e527739fe621be4682516e9
SamaRostom <sama1904157@miuegypt.edu.eg>
2022-12-15 23:03:32 +0200

    Update scores_model.dart

--- Hangtoe/lib/models/scores_model.dart
+++ Hangtoe/lib/models/scores_model.dart
@@ -1,6 +1,9 @@
 import 'package:flutter_application_1/data.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/services/score_service.dart';
 import 'package:google_fonts/google_fonts.dart';
+
+enum Typeofgame{hangman,tic}
 class Scoresmodel{
   //'Rank': 1, 'Name': 'Player', 'Date': '22-Mar-31', 'Score': 7
   int? rankid;
@@ -10,9 +13,10 @@ class Scoresmodel{
 // String updatedDt = newFormat.format(date);
   String? date;
   int? score;
-  String? typeofgame;
+  Typeofgame? typeofgame;
+  // Map? ma;
 
-  Scoresmodel({this.rankid,this.rank,this.playername,this.date,this.score,this.typeofgame});
+  Scoresmodel({this.rankid,this.rank,this.playername,this.date,this.score, this.typeofgame});
 
   // List<DataRow> createRow() {
   //   var rank = Data.topRanks;
@@ -27,4 +31,39 @@ class Scoresmodel{
   //           ]))
   //       .toList();
   // }
+
+  Map<String, dynamic> toMap(){
+    return {
+      "rank": rank,
+      "player_name": playername,
+      "date": date,
+      "score" : score,
+      "typeofgame": typeofgame,
+    };
+  }
+  static Map? setmap(){
+    Map? d = FirebaseCrud.getScores();
+    // ma=d;
+    return d;
+  }
+
+  factory Scoresmodel.fromJson(Map<dynamic, dynamic> json) {
+      return Scoresmodel(
+        rank: json['rank'],
+        playername: json['playername'],
+        date: json['date'],
+        score: json['score'],
+        typeofgame: json['typeofgame'],
+      );
+  }
+
+  Scoresmodel.fromMap(Map<String, dynamic> map)
+    : assert(map['rank'] != null),
+    assert(map['playername'] != null),
+    assert(map['date'] != null),
+    rank = map['rank'],
+    playername = map['playername'],
+    date = map['date'],
+    score = map['score'],
+    typeofgame = map['typeofgame'] == 'hangman' ? Typeofgame.hangman : Typeofgame.tic;
 }
