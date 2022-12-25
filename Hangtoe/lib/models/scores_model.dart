bbca83f74e83c6a29f0e89e3b44c0fde69ff5415
SamaRostom <sama1904157@miuegypt.edu.eg>
2022-12-25 17:45:01 +0200

    Update scores_model.dart

--- Hangtoe/lib/models/scores_model.dart
+++ Hangtoe/lib/models/scores_model.dart
@@ -1,22 +1,35 @@
-import 'package:flutter_application_1/data.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_application_1/services/score_service.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'dart:convert';
 
 enum Typeofgame{hangman,tic}
+List<Scoresmodel> userFromJson(String str) =>
+    List<Scoresmodel>.from(json.decode(str).map((x) => Scoresmodel.fromJson(x)));
+
+String userToJson(List<Scoresmodel> data) =>
+    json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+
 class Scoresmodel{
+
+  Scoresmodel({
+    required this.rankid,
+    required this.rank,
+    required this.playername,
+    required this.date,
+    required this.score, 
+    required this.typeofgame
+  });
+
   //'Rank': 1, 'Name': 'Player', 'Date': '22-Mar-31', 'Score': 7
-  int? rankid;
-  int? rank;
-  String? playername;
+  String rankid;
+  int rank;
+  String playername;
 //   var newFormat = DateFormat("yy-MM-dd");
 // String updatedDt = newFormat.format(date);
-  String? date;
-  int? score;
-  Typeofgame? typeofgame;
+  String date;
+  int score;
+  Typeofgame typeofgame;
   // Map? ma;
 
-  Scoresmodel({this.rankid,this.rank,this.playername,this.date,this.score, this.typeofgame});
+  
 
   // List<DataRow> createRow() {
   //   var rank = Data.topRanks;
@@ -32,38 +45,56 @@ class Scoresmodel{
   //       .toList();
   // }
 
-  Map<String, dynamic> toMap(){
-    return {
-      "rank": rank,
-      "player_name": playername,
-      "date": date,
-      "score" : score,
-      "typeofgame": typeofgame,
-    };
-  }
-  static Map? setmap(){
-    Map? d = FirebaseCrud.getScores();
-    // ma=d;
-    return d;
-  }
+  // Map<String, dynamic> toMap(){
+  //   return {
+  //     "rank": rank,
+  //     "player_name": playername,
+  //     "date": date,
+  //     "score" : score,
+  //     "typeofgame": typeofgame,
+  //   };
+  // }
+  // static Map? setmap(){
+  //   Map? d = FirebaseCrud.getScores();
+  //   // ma=d;
+  //   return d;
+  // }
 
-  factory Scoresmodel.fromJson(Map<dynamic, dynamic> json) {
-      return Scoresmodel(
+  // factory Scoresmodel.fromJson(Map<String, dynamic> json) {
+  //     return Scoresmodel(
+  //       rank: json['rank'],
+  //       playername: json['playername'],
+  //       date: json['date'],
+  //       score: json['score'],
+  //       typeofgame: json['typeofgame'],
+  //     );
+  // }
+
+  factory Scoresmodel.fromJson(Map<String, dynamic> json) => Scoresmodel(
+        rankid: json['rankid'],
         rank: json['rank'],
         playername: json['playername'],
         date: json['date'],
         score: json['score'],
         typeofgame: json['typeofgame'],
       );
-  }
 
-  Scoresmodel.fromMap(Map<String, dynamic> map)
-    : assert(map['rank'] != null),
-    assert(map['playername'] != null),
-    assert(map['date'] != null),
-    rank = map['rank'],
-    playername = map['playername'],
-    date = map['date'],
-    score = map['score'],
-    typeofgame = map['typeofgame'] == 'hangman' ? Typeofgame.hangman : Typeofgame.tic;
+  // Scoresmodel.fromMap(Map<String, dynamic> map)
+  //   : assert(map['rank'] != null),
+  //   assert(map['playername'] != null),
+  //   assert(map['date'] != null),
+  //   rank = map['rank'],
+  //   playername = map['playername'],
+  //   date = map['date'],
+  //   score = map['score'],
+  //   typeofgame = map['typeofgame'] == 'hangman' ? Typeofgame.hangman : Typeofgame.tic;
+
+  Map<String, dynamic> toJson() => {
+        "rankid": rankid,
+        "rank": rank,
+        "playername": playername,
+        "date": date,
+        "score": score,
+        "typeofgame": typeofgame,
+      };
 }
