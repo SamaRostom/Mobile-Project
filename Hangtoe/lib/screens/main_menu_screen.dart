e7bbab4fe7dd95d24a39d6ac2f680ea47a94a27c
YaraLotfy <96872220+YaraLotfy@users.noreply.github.com>
2023-01-06 19:01:12 +0200

    Create main_menu_screen.dart

---
+++ Hangtoe/lib/screens/main_menu_screen.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/Screens/create_room_screen.dart';
+import 'package:flutter_application_1/Screens/join_room_screen.dart';
+import 'package:flutter_application_1/responsive/responsive.dart';
+import 'package:flutter_application_1/widgets/custom_button.dart';
+
+class MainMenuScreen extends StatelessWidget {
+  // static String routeName = '/main-menu';
+  const MainMenuScreen({Key? key}) : super(key: key);
+
+  // void createRoom(BuildContext context) {
+  //   Navigator.pushNamed(context, CreateRoomScreen.routeName);
+  // }
+
+  // void joinRoom(BuildContext context) {
+  //   Navigator.pushNamed(context, JoinRoomScreen.routeName);
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Responsive(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            IconButton(
+              icon: const Icon(Icons.arrow_back),
+              color: Colors.white,
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+            CustomButton(
+              // onTap: () => createRoom(context),
+              onTap: () {
+                Navigator.pushNamed(context, '/create_room_screen');
+              },
+              text: 'Create Room',
+            ),
+            const SizedBox(height: 20),
+            CustomButton(
+              // onTap: () => joinRoom(context),
+              onTap: () {
+                Navigator.pushNamed(context, '/join_room_screen');
+              },
+              text: 'Join Room',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
