5cf7933ce5d37ff2d7efd999f809ee2527ac348d
YaraLotfy <96872220+YaraLotfy@users.noreply.github.com>
2023-01-06 19:01:06 +0200

    Create join_room_screen.dart

---
+++ Hangtoe/lib/screens/join_room_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class JoinRoomScreen extends StatefulWidget {
+  // static String routeName = '/join-room';
+  const JoinRoomScreen({Key? key}) : super(key: key);
+
+  @override
+  State<JoinRoomScreen> createState() => _JoinRoomScreenState();
+}
+
+class _JoinRoomScreenState extends State<JoinRoomScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
