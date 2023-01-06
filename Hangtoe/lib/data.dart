b2a9f12cbfe9cec23de7cfef7449227844f2249a
SamaRostom <sama1904157@miuegypt.edu.eg>
2023-01-06 12:27:36 +0200

    Update data.dart

--- Hangtoe/lib/data.dart
+++ Hangtoe/lib/data.dart
@@ -27,19 +27,19 @@ class Data {
 
   // ignore: non_constant_identifier_names
   static List<Map> Hangscore = [
-    {'rank': 1, 'player_name': 'Player', 'date': '22-Mar-31', 'score': 7},
-    {'rank': 2, 'player_name': 'Ahmed', 'date': '22-Apr-1', 'score': 6},
-    {'rank': 3, 'player_name': 'Noor', 'date': '22-May-12', 'score': 5},
-    {'rank': 4, 'player_name': 'Pl', 'date': '22-Mar-26', 'score': 2},
-    {'rank': 5, 'player_name': 'Sara', 'date': '22-Feb-12', 'score': 1},
+    {'rank': 1, 'player_name': 'Player', 'date': '22-Mar-31', 'score': 7, 'typeofgame':'hangman'},
+    {'rank': 2, 'player_name': 'Ahmed', 'date': '22-Apr-1', 'score': 6,'typeofgame':'hangman'},
+    {'rank': 3, 'player_name': 'Noor', 'date': '22-May-12', 'score': 5,'typeofgame':'hangman'},
+    {'rank': 4, 'player_name': 'Pl', 'date': '22-Mar-26', 'score': 2,'typeofgame':'hangman'},
+    {'rank': 5, 'player_name': 'Sara', 'date': '22-Feb-12', 'score': 1,'typeofgame':'hangman'},
   ];
 
   static List<Map> xoscore = [
-    {'rank': 1, 'player_name': 'play', 'date': '22-Mar-31', 'score': 7},
-    {'rank': 2, 'player_name': 'Ahmed', 'date': '22-Apr-1', 'score': 4},
-    {'rank': 3, 'player_name': 'Noor', 'date': '22-May-12', 'score': 3},
-    {'rank': 4, 'player_name': 'Pl', 'date': '22-Mar-26', 'score': 1},
-    {'rank': 5, 'player_name': 'Sara', 'date': '22-Feb-12', 'score': 1},
+    {'rank': 1, 'player_name': 'play', 'date': '22-Mar-31', 'score': 7,'typeofgame':'xo'},
+    {'rank': 2, 'player_name': 'Ahmed', 'date': '22-Apr-1', 'score': 4,'typeofgame':'xo'},
+    {'rank': 3, 'player_name': 'Noor', 'date': '22-May-12', 'score': 3,'typeofgame':'xo'},
+    {'rank': 4, 'player_name': 'Pl', 'date': '22-Mar-26', 'score': 1,'typeofgame':'xo'},
+    {'rank': 5, 'player_name': 'Sara', 'date': '22-Feb-12', 'score': 1,'typeofgame':'xo'},
   ];
 
   // Random rnd = Random();
