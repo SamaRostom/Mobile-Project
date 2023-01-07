2293b8ce73341dec0850fdc3d5e09d724381bedb
SamaRostom <sama1904157@miuegypt.edu.eg>
2023-01-07 23:12:24 +0200

    Update Signup.dart

--- Hangtoe/lib/screens/Signup.dart
+++ Hangtoe/lib/screens/Signup.dart
@@ -1,18 +1,53 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/main.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:flutter_application_1/data.dart';
+import 'package:flutter_application_1/Utils/data.dart';
+import '../Utils/validations.dart';
+import '../models/user_model.dart';
+import '../providers/user_provider.dart';
+import '../services/user_service.dart';
 
-class Signup extends StatefulWidget {
+class Signup extends ConsumerStatefulWidget {
   const Signup({super.key});
 
   @override
   // ignore: library_private_types_in_public_api
-  _SignupState createState() => _SignupState();
+  ConsumerState<Signup> createState() => _SignupState();
 }
 
-class _SignupState extends State<Signup> {
+class _SignupState extends ConsumerState<Signup> {
   final _formKey = GlobalKey<FormState>();
+  final _nameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  
+  @override
+  void dispose() {
+    _nameController.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  //byd5al el 7aga fel firebas bas mesh byro7 3ala page login
+  void redirectToLogin() {
+    final user = FirebaseAuth.instance.currentUser!;
+    UserHelper.saveUser(user);
+    //Salah Way
+    // Future userData = UserHelper().getUserData();
+    // ref.read(userDataProviderRepository.notifier).state = userData;
+    // Navigator.of(context).pushNamed('/home');
+
+    UserHelper().getNewUserData().then((value) {
+      UserModel user = UserModel.fromSnapshot(value);
+      ref.read(newUserDataProivder.notifier).state = user;
+      Navigator.of(context).pushReplacementNamed('/Login');
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
     // final double height = MediaQuery.of(context).size.height;
@@ -40,14 +75,16 @@ class _SignupState extends State<Signup> {
             child: Column(
               children: [
                 Image.asset('assets/Signup.png', width: 200),
-                const SizedBox(height: 50),
+                const SizedBox(height: 20),
+                //username
                 TextFormField(
                   autofocus: true,
+                  controller: _nameController,
                   validator: (val) {
-                    if (val != null && val.isNotEmpty) {
+                    if (val != null && isName(val)) {
                       return null;
                     } else {
-                      return 'Please enter a username';
+                      return 'Please enter a valid username';
                     }
                   },
                   style: const TextStyle(color: Colors.white),
@@ -57,14 +94,36 @@ class _SignupState extends State<Signup> {
                     hintStyle: TextStyle(color: Colors.white),
                   ),
                 ),
-                const SizedBox(height: 50),
+                const SizedBox(height: 20),
+                //email
                 TextFormField(
                   autofocus: true,
+                  controller: _emailController,
                   validator: (val) {
-                    if (val != null && val.isNotEmpty) {
+                    if (val != null && isEmail(val)) {
                       return null;
                     } else {
-                      return 'Please enter a password';
+                      return 'Please enter a valid email';
+                    }
+                  },
+                  style: const TextStyle(color: Colors.white),
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Email',
+                    hintStyle: TextStyle(color: Colors.white),
+                  ),
+                ),
+                const SizedBox(height: 20),
+
+                //password
+                TextFormField(
+                  autofocus: true,
+                  controller: _passwordController,
+                  validator: (val) {
+                    if (val != null && isPassword(val) ) {
+                      return null;
+                    } else {
+                      return 'Please enter At Least 8 characters one letter and one number';
                     }
                   },
                   decoration: const InputDecoration(
@@ -76,10 +135,14 @@ class _SignupState extends State<Signup> {
                 const SizedBox(height: 20),
                 ButtonBar(
                   children: <Widget>[
+                    //signup button
                     ElevatedButton(
                       onPressed: () {
                         if (_formKey.currentState!.validate()) {
-                          Navigator.pushNamed(context, '/Login');
+                          // Navigator.pushNamed(context, '/Login');
+                          // UserHelper().signUp(
+                          //       context, _nameController, _emailController, _passwordController);
+                          redirectToLogin();      
                         } 
                         // Navigator.pushNamed(context, '/Login');
                       },
