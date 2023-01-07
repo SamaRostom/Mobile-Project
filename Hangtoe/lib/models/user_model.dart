6dbc851142c8d1d674aee6fbdd5563826b78266c
SamaRostom <sama1904157@miuegypt.edu.eg>
2023-01-07 23:11:52 +0200

    Create user_model.dart

---
+++ Hangtoe/lib/models/user_model.dart
@@ -0,0 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class UserModel {
+  String username;
+  String email;
+  // String password;
+  UserModel(this.username, this.email);
+  int compareTofunction(UserModel user) => username.compareTo(user.username);
+
+  factory UserModel.fromSnapshot(DocumentSnapshot snapshot) {
+    return UserModel(snapshot.get('username'), snapshot.get('email'));
+  }
+}
