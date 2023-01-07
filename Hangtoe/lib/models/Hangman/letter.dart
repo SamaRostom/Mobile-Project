1d26d1a340f6440e297c21cd615b25cb133b3efe
SamaRostom <sama1904157@miuegypt.edu.eg>
2023-01-07 23:11:50 +0200

    Update letter.dart

--- Hangtoe/lib/models/Hangman/letter.dart
+++ Hangtoe/lib/models/Hangman/letter.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/data.dart';
+import 'package:flutter_application_1/Utils/data.dart';
 
 Widget letter(String character, bool hidden) {
   return Container(
