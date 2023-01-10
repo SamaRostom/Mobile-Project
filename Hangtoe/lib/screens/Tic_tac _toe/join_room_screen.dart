1a60c1adba892eeca9a400effa3265770d01beb4
YaraLotfy <96872220+YaraLotfy@users.noreply.github.com>
2023-01-10 18:11:25 +0200

    Update join_room_screen.dart

--- Hangtoe/lib/screens/Tic_tac _toe/join_room_screen.dart
+++ Hangtoe/lib/screens/Tic_tac _toe/join_room_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_application_1/widgets/custom_button.dart';
+import 'package:flutter_application_1/widgets/custom_text.dart';
 
 class JoinRoomScreen extends StatefulWidget {
   // static String routeName = '/join-room';
@@ -11,6 +14,93 @@ class JoinRoomScreen extends StatefulWidget {
 class _JoinRoomScreenState extends State<JoinRoomScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return SafeArea(
+      child: Scaffold(
+        body: ListView(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 20,
+          ),
+          children: [
+            IconButton(
+              icon: const Icon(Icons.arrow_back),
+              color: Colors.white,
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: const <Widget>[
+                CustomText(
+                  shadows: [
+                    Shadow(
+                      blurRadius: 40,
+                      color: Color.fromARGB(255, 208, 211, 213),
+                    ),
+                  ],
+                  text: 'Join Room',
+                  fontSize: 70,
+                ),
+                SizedBox(
+                  height: 100,
+                ),
+                TextField(
+                  style: TextStyle(color: Colors.white),
+                  decoration: InputDecoration(
+                    fillColor: Colors.white,
+                    focusColor: Colors.white,
+                    hoverColor: Colors.white,
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(width: 3, color: Colors.white),
+                    ),
+                    hintText: 'Enter your nickname',
+                    hintStyle: TextStyle(color: Colors.white),
+                  ),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                TextField(
+                  style: TextStyle(color: Colors.white),
+                  decoration: InputDecoration(
+                    fillColor: Colors.white,
+                    focusColor: Colors.white,
+                    hoverColor: Colors.white,
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(width: 3, color: Colors.white),
+                    ),
+                    hintText: 'Enter Game ID',
+                    hintStyle: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            SizedBox(
+              height: 50, //height of button
+              width: 120,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                      //to set border radius to button
+                      borderRadius: BorderRadius.circular(20)),
+                ),
+                onPressed: () {},
+                child: Text('Join',
+                    style: GoogleFonts.patrickHand(
+                      fontSize: 25,
+                    )),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
