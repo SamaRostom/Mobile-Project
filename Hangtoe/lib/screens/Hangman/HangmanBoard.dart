88fb0f628bcea15b8fab298e29c8414093d84f66
ReemAbdulraouf <76842378+ReemAbdulraouf@users.noreply.github.com>
2023-01-16 00:19:54 +0200

    Update HangmanBoard.dart

--- Hangtoe/lib/screens/Hangman/HangmanBoard.dart
+++ Hangtoe/lib/screens/Hangman/HangmanBoard.dart
@@ -12,14 +12,14 @@ import 'package:flutter_application_1/models/Hangman/figure_image.dart';
 import 'package:flutter_application_1/models/Hangman/HangmanGame.dart';
 // import 'package:flutter_application_1/drawing.dart';
 
-
 const TextStyle activeWordStyle = TextStyle(
   fontSize: 30.0,
   letterSpacing: 5.0,
 );
 
-
 class HangmanBoard extends StatefulWidget {
+  static const routeName = "/hangmanboard-screen";
+
   final HangmanGame obj;
   const HangmanBoard({super.key, required this.obj});
   // HangmanBoard(this.obj);
@@ -31,7 +31,6 @@ class HangmanBoard extends StatefulWidget {
   // mn el code el gded fe hangmanui
   State<StatefulWidget> createState() => _HangmanBoardState();
 }
-
 
 class _HangmanBoardState extends State<HangmanBoard> {
   //choosing the game word
@@ -55,13 +54,11 @@ class _HangmanBoardState extends State<HangmanBoard> {
     newGame();
   }
 
-
   void updateWordDisplay(String word) {
     setState(() {
       activeWord = word;
     });
   }
-
 
   void updateGallowsImage(int wrongGuessCount) {
     setState(() {
@@ -69,7 +66,6 @@ class _HangmanBoardState extends State<HangmanBoard> {
       activeImage = Data.progressImages[wrongGuessCount];
     });
   }
-
 
   void win([_]) {
     setState(() {
@@ -94,7 +90,6 @@ class _HangmanBoardState extends State<HangmanBoard> {
     });
   }
 
-
   Widget renderBottomContent() {
     if (showNewGame) {
       return ElevatedButton(
@@ -102,100 +97,100 @@ class _HangmanBoardState extends State<HangmanBoard> {
         onPressed: newGame,
       );
     }
-  // }
+    // }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Hangman"),
-        elevation: 0,
-        centerTitle: true,
-      ),
-
-
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Center(
-            child: Stack(
-              children: [
-                //figure will be built according to the number of tries
-                figureImage(Data.tries >= 0, "assets/hang.png"),
-                figureImage(Data.tries >= 1, "assets/head.png"),
-                figureImage(Data.tries >= 2, "assets/body.png"),
-                figureImage(Data.tries >= 3, "assets/ra.png"),
-                figureImage(Data.tries >= 4, "assets/la.png"),
-                figureImage(Data.tries >= 5, "assets/rl.png"),
-                figureImage(Data.tries >= 6, "assets/ll.png"),
-              ],
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text("Hangman"),
+          elevation: 0,
+          centerTitle: true,
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Center(
+              child: Stack(
+                children: [
+                  //figure will be built according to the number of tries
+                  figureImage(Data.tries >= 0, "assets/hang.png"),
+                  figureImage(Data.tries >= 1, "assets/head.png"),
+                  figureImage(Data.tries >= 2, "assets/body.png"),
+                  figureImage(Data.tries >= 3, "assets/ra.png"),
+                  figureImage(Data.tries >= 4, "assets/la.png"),
+                  figureImage(Data.tries >= 5, "assets/rl.png"),
+                  figureImage(Data.tries >= 6, "assets/ll.png"),
+                ],
+              ),
             ),
-          ),
 
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children:
+                  //split the word letter by letter
+                  activeWord
+                      .split('')
+                      .map((e) => letter(e.toUpperCase(),
+                          !Data.selectedChar.contains(e.toUpperCase())))
+                      .toList(),
+            ),
 
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children:
-            //split the word letter by letter
-                activeWord.split('')
-                .map((e) => letter(e.toUpperCase(),
-                    !Data.selectedChar.contains(e.toUpperCase())))
-                .toList(),
-          ),
-
-          //Game keyboard
-          SizedBox(
-            height: 250.0,
-            child: GridView.count(
-              crossAxisCount: 7,
-              crossAxisSpacing: 8.0,
-              padding: EdgeInsets.all(8.0),
-              children: Data.alphabets.map((e) {
-                return RawMaterialButton(
-                  onPressed: Data.selectedChar.contains(e)
-                      ? null // check that we didn't selected the button before
-                      : () {
-                          setState(() {
-                            Data.selectedChar.add(e);
-                            // ignore: avoid_print
-                            print(Data.selectedChar);
-                            //  count wrong guesses
-                            if (!activeWord.split('').contains(e.toUpperCase())) {
-                              Data.tries++;
-                            }
-                            //  return to home page when the draw is fully drawn
-                            if(Data.tries == 6){
-                              Navigator.pushNamed(context, '/');
-                            }
-                          });
-                        },
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(4.0),
-                  ),
-                  // ignore: sort_child_properties_last
-                  child: Text(
-                    e,
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 30.0,
-                      fontWeight: FontWeight.bold,
+            //Game keyboard
+            SizedBox(
+              height: 250.0,
+              child: GridView.count(
+                crossAxisCount: 7,
+                crossAxisSpacing: 8.0,
+                padding: EdgeInsets.all(8.0),
+                children: Data.alphabets.map((e) {
+                  return RawMaterialButton(
+                    onPressed: Data.selectedChar.contains(e)
+                        ? null // check that we didn't selected the button before
+                        : () {
+                            setState(() {
+                              Data.selectedChar.add(e);
+                              // ignore: avoid_print
+                              print(Data.selectedChar);
+                              //  count wrong guesses
+                              if (!activeWord
+                                  .split('')
+                                  .contains(e.toUpperCase())) {
+                                Data.tries++;
+                              }
+                              //  return to home page when the draw is fully drawn
+                              if (Data.tries == 6) {
+                                Navigator.pushNamed(context, '/');
+                              }
+                            });
+                          },
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(4.0),
                     ),
-                  ),
-                  fillColor: Data.selectedChar.contains(e)
-                      ? Colors.black87
-                      : Colors.blue,
-                );
-              }).toList(),
-            ),
-          )
-        ],
-      ),
-    );
-  }
+                    // ignore: sort_child_properties_last
+                    child: Text(
+                      e,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 30.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    fillColor: Data.selectedChar.contains(e)
+                        ? Colors.black87
+                        : Colors.blue,
+                  );
+                }).toList(),
+              ),
+            )
+          ],
+        ),
+      );
+    }
+
     throw UnimplementedError();
   }
-  
 
   // kan feh error fe line 34 (class _HangmanBoardState extends State<HangmanBoard> {)
   @override
@@ -203,5 +198,4 @@ class _HangmanBoardState extends State<HangmanBoard> {
     // TODO: implement build
     throw UnimplementedError();
   }
-
 }
