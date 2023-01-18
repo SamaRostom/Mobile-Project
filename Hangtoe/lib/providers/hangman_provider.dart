17e4666b8dc9312929e2fb479ae946e0d8ef21dd
SamaRostom <sama1904157@miuegypt.edu.eg>
2023-01-18 20:33:11 +0200

    Update hangman_provider.dart

--- Hangtoe/lib/providers/hangman_provider.dart
+++ Hangtoe/lib/providers/hangman_provider.dart
@@ -4,3 +4,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 final wordProivder = StateProvider<String>((ref) {
   return "";
 });
+
+final timeProivder = StateProvider<int>((ref) {
+  return 0;
+});
