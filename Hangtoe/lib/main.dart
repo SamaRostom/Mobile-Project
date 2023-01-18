272c30e2cfedf7056733f21fb969551536dbba9d
SalmaSalahh <salma1905780@miuegypt.edu.eg>
2023-01-18 20:46:14 +0200

    Update gamehome.dart

--- Hangtoe/lib/main.dart
+++ Hangtoe/lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/screens/Hangman2/categories.dart';
-import 'package:flutter_application_1/screens/Hangman2/game_screen.dart';
-import 'package:flutter_application_1/screens/Hangman2/lose_screen.dart';
-import 'package:flutter_application_1/screens/Hangman2/type_word_screen.dart';
-import 'package:flutter_application_1/screens/Hangman2/win_screen.dart';
+import 'package:flutter_application_1/screens/Hangman/categories.dart';
+import 'package:flutter_application_1/screens/Hangman/game_screen.dart';
+import 'package:flutter_application_1/screens/Hangman/lose_screen.dart';
+import 'package:flutter_application_1/screens/Hangman/type_word_screen.dart';
+import 'package:flutter_application_1/screens/Hangman/win_screen.dart';
 import 'package:flutter_application_1/screens/Home.dart';
 import 'package:flutter_application_1/screens/Tic_tac%20_toe/singleplayerAI.dart';
 import 'package:flutter_application_1/screens/scores.dart';
@@ -43,7 +43,6 @@ class MyApp extends StatelessWidget {
         NoPlayers.routeName: (context) => const NoPlayers(),
         Gamehome.routeName: (context) => const Gamehome(),
         HowToPlay.routeName: (context) => const HowToPlay(),
-        // '/HangmanBoard': (context) => const HangmanBoard(),
         PName.routeName: (context) => const PName(),
         SinglepalyerAI.routeName: (context) => const SinglepalyerAI(),
         Twoplayers.routeName: (context) => const Twoplayers(),
