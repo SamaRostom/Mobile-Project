ce5e37138c483a893d90eac4c32d6a3f623f7cc3
YaraLotfy <96872220+YaraLotfy@users.noreply.github.com>
2023-01-18 20:50:28 +0200

    Update custom_button.dart

--- Hangtoe/lib/widgets/custom_button.dart
+++ Hangtoe/lib/widgets/custom_button.dart
@@ -12,11 +12,11 @@ class CustomButton extends StatelessWidget {
     final width = MediaQuery.of(context).size.width;
     return ElevatedButton(
       onPressed: onTap,
+      style: ElevatedButton.styleFrom(minimumSize: Size(width, 50)),
       child: Text(text,
           style: const TextStyle(
             fontSize: 16,
           )),
-      style: ElevatedButton.styleFrom(minimumSize: Size(width, 50)),
     );
   }
 }
